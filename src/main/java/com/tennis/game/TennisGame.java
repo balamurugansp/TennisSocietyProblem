c7852a7f3dc3c0431b35e6d9c5a47f8769260860
balamurugansp <spbalamurugan@gmail.com>
2016-12-02 19:29:45 +0530

    TennisGameProblem

---
+++ src/main/java/com/tennis/game/TennisGame.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.tennis.game;
+
+/**
+ * @author bsengode
+ *
+ */
+public interface TennisGame {
+
+	public void wonPoint(String playerName);
+
+	public String getScore();
+
+}
